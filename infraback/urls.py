59c1bfefd859dc7b0b5324f724746d6d57732783
Mohamed-Ali-Bego <mohamedalibego11@gmail.com>
2024-11-16 18:17:54 +0200

    sic

--- infraback/urls.py
+++ infraback/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('accounts/' , include('accounts.urls')),
     path('products/' , include('products.urls') ),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
